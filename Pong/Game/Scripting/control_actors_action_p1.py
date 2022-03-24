ed24a23ca22985c166a4a1a2461cc120b028f08a
Colby Patterson <cpatt@byui.edu>
2022-03-23 21:53:57 -0600

    Added control_actors_action for p1 and p2

--- Pong/Game/Scripting/control_actors_action_p1.py
+++ Pong/Game/Scripting/control_actors_action_p1.py
@@ -12,3 +12,11 @@ class ControlActorsAction(Action):
     def execute(self, cast, script):
 
         pass
+
+        #player 1 up
+        #if self._keyboard_service.is_key_down('w'):
+            #self._direction = Point(0, -constants.CELL_SIZE)
+
+        #player 1 down
+        #if self._keyboard_service.is_key_down('s'):
+            #self._direction = Point(0, constants.CELL_SIZE)
