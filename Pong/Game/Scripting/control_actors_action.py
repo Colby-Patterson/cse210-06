650baec96327fbecd9f7465251313be66eadd4fc
Colby Patterson <cpatt@byui.edu>
2022-03-23 21:48:21 -0600

    Working on control_actors_action

--- Pong/Game/Scripting/control_actors_action.py
+++ Pong/Game/Scripting/control_actors_action.py
@@ -1,1 +1,14 @@
+#import constants
+#from game.shard.point import Point
 from game.scripting.action import Action
+
+class ControlActorsAction(Action):
+
+    def __init__ (self, keyboard_service):
+
+        self._keyboard_service = keyboard_service
+        #self._direction = Point(0, constants.CELL_SIZE)
+
+    def execute(self, cast, script):
+
+        pass
