98448dc7227fc42a31b947bbc1807c4f861a4d57
Kovekk <fishm3001@gmail.com>
2022-03-26 19:54:24 -0600

    moving the call to scene manager to director

--- Pong/Game/director/director.py
+++ Pong/Game/director/director.py
@@ -1,3 +1,7 @@
+from game.scripting.script import Script
+from game.casting.cast import Cast
+from game.director.scene_manager import SceneManager
+
 class Director:
     """A person who directs the game. 
     
@@ -14,22 +18,29 @@ class Director:
             video_service (VideoService): An instance of VideoService.
         """
         self._video_service = video_service
+        self._script = Script()
+        self._cast = Cast()
+        self.scene_manager = SceneManager()
         
-    def start_game(self, cast, script):
+    def start_game(self):
         """Starts the game using the given cast and script. Runs the main game loop.
 
         Args:
             cast (Cast): The cast of actors.
             script (Script): The script of actions.
         """
+
+        self.scene_manager.prepare_scene("menu")
+
+
         self._video_service.open_window()
         while self._video_service.is_window_open():
-            self._execute_actions("input", cast, script)
-            self._execute_actions("update", cast, script)
-            self._execute_actions("output", cast, script)
+            self._execute_actions("input", self._cast, self._script)
+            self._execute_actions("update", self._cast, self._script)
+            self._execute_actions("output", self._cast, self._script)
         self._video_service.close_window()
 
-    def _execute_actions(self, group, cast, script):
+    def _execute_actions(self, group):
         """Calls execute for each action in the given group.
         
         Args:
@@ -37,6 +48,10 @@ class Director:
             cast (Cast): The cast of actors.
             script (Script): The script of actions.
         """
-        actions = script.get_actions(group)    
+        actions = self._script.get_actions(group)    
         for action in actions:
-            action.execute(cast, script)   
+            action.execute(self._cast, self._script)
+
+
+    def change_scene(self, scene):
+        self.scene_manager.prepare_scene(scene)
